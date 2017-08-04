6cc68ad6b0af106fbf9e702586cca30d0e9b8684
zhang2015 <zhang_tracy_jack@163.com>
2017-08-04 17:37:27 +0800

    login

---
+++ src/components/login/log_in.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="login_box">
+        <ul class="login_box_tab">
+            <li @click="changeTab('type1')" :class="loginType=='type1'? 'active':''">普通登录</li>
+            <li @click="changeTab('type2')" :class="loginType=='type2'? 'active':''">手机动态密码登录</li>
+        </ul>
+        <section v-if="loginType == 'type1'">
+            <input type="text" class="login_input" v-model="phone" placeholder="请输入您的手机号">
+            <input type="password" class="login_input" v-model="pass" placeholder="请输入您的密码">
+            <div class="twoItem">
+                <input type="text" class="login_input smscode_input" v-model="imgcode" placeholder="请输入右侧验证码">
+                <span class="imgCode">
+    
+                </span>
+            </div>
+        </section>
+        <section v-if="loginType == 'type2'">
+            <input type="text" class="login_input" v-model="phone" placeholder="请输入您的手机号">
+            <div class="twoItem">
+                <input type="text" class="login_input smscode_input" v-model="smscode" placeholder="请输入手机验证码">
+                <span class="smscode_btn">获取验证码</span>
+            </div>
+        </section>
+        <p class="left_type">
+            <span class="icon-check"></span>保持一周登录状态
+        </p>
+        <span class="sign_btn">登录</span>
+        <p class="between_type">
+            <a>免费注册</a>
+            <a>忘记密码?</a>
+        </p>
+        <p class="center_type">使用合作网站登录</p>
+        <div class="center_type loginway">
+            <div>
+                <img src="./image/weixin.png" />
+                <span>微信登录</span>
+            </div>
+            <div>
+                <img src="./image/weibo.png" />
+                <span>微博登录</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    data() {
+        return {
+            loginType: 'type1',
+            phone: '',
+            smscode: '',
+            pass: '',
+            imgcode: ''
+        }
+    },
+    methods: {
+        changeTab: function (value) {
+            this.loginType = value;
+        }
+    }
+}
+</script>
+
+<style media="screen">
+.login_box_tab {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.login_box_tab li {
+    width: 50%;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    background: #ccc;
+    border-top: 3px solid transparent;
+}
+
+.login_box_tab li:nth-child(1) {
+    border-right: 1px solid #f5f5f5;
+    border-bottom: 1px solid #f5f5f5;
+}
+
+.login_box_tab li:nth-child(2) {
+    border-bottom: 1px solid #f5f5f5;
+}
+
+.login_box_tab li.active {
+    border-top: 3px solid rebeccapurple;
+}
+</style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+        <loginNav></loginNav>
+        <div class="main_wrapper">
+            <div class="login_main">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+import loginNav from '../loginNav/loginNav'
+
+export default {
+    components: {
+        loginNav
+    }
+}
+</script>
+
+<style media="screen">
+.login_main {
+    display: flex;
+    width: 100%;
+    height: 300px;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.login_box {
+    width: 300px;
+    height: auto;
+    border: 1px solid #f5f5f5;
+}
+
+.login_box section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.login_input {
+    width: 200px;
+    height: 25px;
+    margin: 5px auto;
+    padding-left: 20px;
+    border: 1px solid #f5f5f5;
+}
+
+.twoItem {
+    display: flex;
+    width: 220px;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.smscode_input {
+    width: 120px;
+    margin: 5px 0;
+}
+
+.smscode_btn {
+    width: 70px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+}
+
+.imgCode {
+    width: 70px;
+    height: 25px;
+}
+
+.left_type,
+.between_type,
+.center_type {
+    display: flex;
+    width: 220px;
+    align-items: center;
+    margin: auto;
+}
+
+.left_type {
+    justify-content: flex-start;
+}
+
+.center_type {
+    justify-content: center;
+}
+
+.between_type {
+    justify-content: space-between;
+}
+.sign_btn{
+    display: block;
+    width: 220px;
+    height: 25px;
+    margin: auto;
+    line-height: 25px;
+    text-align: center;
+    border-radius: 5px;
+}
+.loginway div{
+    display: flex;
+    width: 100px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.loginway div img{
+    width: 50px;
+    height: 50px;
+}
+.login_head{
+    width: 100%;
+    height: 30px;
+    background: #f5f5f5;
+}
+</style>
